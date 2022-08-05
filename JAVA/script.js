129960e4eb1d36b524e197a58cdb9f8201726574
lara-bozzano <larabozzano@tdf.edu.ar>
2022-08-05 00:56:52 -0300

    boton clicks

--- JAVA/script.js
+++ JAVA/script.js
@@ -1,3 +1,21 @@
-function saludar() {
-    alert("Hola!");
+    "use strict";
+    
+    let contador = 0;
+
+
+    function clicks() {
+    contador++;
+    console.log("Hiciste " + contador + " clicks");
     }
+
+    let btn_clicks = document.getElementById("btn_clicks");
+    btn_clicks.addEventListener("click", clicks);
+
+
+
+    function contarClicks() {
+        alert("Hiciste " + contador + " clicks");
+        } 
+
+    let btn_mostrar = document.getElementById("btn_mostrar");
+    btn_mostrar.addEventListener("click", contarClicks);
