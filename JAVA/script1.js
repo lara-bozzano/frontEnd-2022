5ebb37e240d1a0b9684455743731b91e85e49317
lara-bozzano <larabozzano@tdf.edu.ar>
2022-08-06 12:41:54 -0300

    java ejercicio input saludar

---
+++ JAVA/script1.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let btn_nombre = document.getElementById("btn_nombre");
+btn_nombre.addEventListener("click", mostrar);
+
+function mostrar(){
+    let nombre = "anonimo";
+    let inputNombre = document.getElementById("inputNombre");
+    nombre = inputNombre.value;
+
+    alert("Bienvenido " + nombre);
+
+}
